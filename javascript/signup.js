d82673c32e70db18a06d3a5324bb5383f3388b19
shalin <shalinshah1993@gmail.com>
2014-03-25 01:46:07 +0530

    Initial Commit

---
+++ javascript/signup.js
@@ -0,0 +1,37 @@
+//This function is used to validate the Email Address entered.
+function validateEmail(email){
+	var regexString = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return regexString.test(email);
+}
+
+//This is the helper function to validate the Email entered. 
+function validate(){
+	var emaildAddress = document.getElementById("indexPageEmailId").value;
+	var validText = document.getElementById("indexPageValidateText");
+	var styledString = "";
+
+	if(emaildAddress === ""){
+		styledString.fontcolor("black");
+		styledString = "";
+		validText.innerHTML = styledString;
+		return;
+	}
+
+	if(validateEmail(emaildAddress)){
+		styledString = "Valid Email";
+		validText.innerHTML = styledString.fontcolor("green");
+	}else{
+		styledString = "Invalid Email";
+		validText.innerHTML = styledString.fontcolor("red");
+	}
+	return;
+}
+
+//This is the function to clear all the fields of the sign-up form.
+function resetFields(){
+	//document.write("asfdasfa");
+	document.getElementById("indexPageEmailId").value = "";
+	document.getElementById("indexPageName").value = "";
+	document.getElementById("indexPageValidateText").innerHTML = "";
+	document.getElementById("indexPageRememberCheck").checked = false;
+}
